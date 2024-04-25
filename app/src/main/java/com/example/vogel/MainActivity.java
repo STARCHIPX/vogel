40bd98c61e9d4f51e4fab3cd230f6b28f5abe9c1
dominikgodau <domink.godau@hotmail.de>
2024-04-25 10:46:04 +0200

    Kommentar hinzu

--- app/src/main/java/com/example/vogel/MainActivity.java
+++ app/src/main/java/com/example/vogel/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    //Godau coment
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
