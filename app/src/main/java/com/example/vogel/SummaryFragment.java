66b21f8581d8800ab2fd574f5b1045efc1a7ce1d
nora <na.nora@gmx.de>
2024-06-03 19:06:08 +0200

    - Update Fragment_Summary - Update SummaryFragment - Update ActionFragment

--- app/src/main/java/com/example/vogel/SummaryFragment.java
+++ app/src/main/java/com/example/vogel/SummaryFragment.java
@@ -12,15 +12,12 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 
 public class SummaryFragment extends Fragment {
 
     private TextView selectedOptionTextView;
     private TextView selectedTimeOfDayTextView;
-    private Button saveToDatabaseButton;
-
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -33,7 +30,6 @@ public class SummaryFragment extends Fragment {
 
         selectedOptionTextView = view.findViewById(R.id.selectedOptionTextView);
         selectedTimeOfDayTextView = view.findViewById(R.id.selectedTimeOfDayTextView);
-        saveToDatabaseButton = view.findViewById(R.id.buttonSaveToDatabase);
 
         // Empfange die übergebenen Argumente
         Bundle args = getArguments();
@@ -44,34 +40,8 @@ public class SummaryFragment extends Fragment {
             selectedOptionTextView.setText("Selected Option: " + selectedOption);
             selectedTimeOfDayTextView.setText("Selected Time of Day: " + selectedTimeOfDay);
         }
-
-        // OnClickListener für den Button setzen
-        saveToDatabaseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveToDatabase();
-            }
-        });
     }
 
-    private void saveToDatabase() {
-        // Datenbankzugriff und Schreiboperationen hier durchführen
-        DatabaseHelper dbHelper = new DatabaseHelper(getContext());
-        String selectedOption = selectedOptionTextView.getText().toString();
-        String selectedTimeOfDay = selectedTimeOfDayTextView.getText().toString();
-
-        boolean isInserted = dbHelper.insertSelection(selectedOption, selectedTimeOfDay);
-        if (isInserted) {
-            Toast.makeText(getContext(), "Selection saved to database", Toast.LENGTH_SHORT).show();
-            FragmentManager fragmentManager = getParentFragmentManager();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, new OverviewFragment())
-                    .addToBackStack(null)
-                    .commit();
-        } else {
-            Toast.makeText(getContext(), "Error saving selection to database", Toast.LENGTH_SHORT).show();
-        }
-    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
